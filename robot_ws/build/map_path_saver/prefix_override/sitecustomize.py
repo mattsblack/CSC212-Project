2634d28202ce853c62b796b6546f2ca692a0bfb1
mattsblack <47698245+mattsblack@users.noreply.github.com>
2025-04-03 12:17:02 -0400

    reorganize files
    delete old log, build, and install files. move source files to root directory.

--- robot_ws/build/map_path_saver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/matt/robot_ws/install/map_path_saver'
--- robot_ws/src/build/map_path_saver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/matt/CSC212-Roomba-Project/robot_ws/src/install/map_path_saver'
